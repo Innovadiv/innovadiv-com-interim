d715390a7ea1c1d810d5e0c437ea7e5713a8a061
Matt Lo <mattlo.developer@gmail.com>
2015-02-11 19:13:08 -0600

    added logo

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>Innovadiv - Entertainment Consultation</h1><p>Contact <a href="mailto:lscullark@innovadiv.com">lscullark@innovadiv.com</a> for more details</p>');
+  res.end('<p><img style="max-height: 60px;" src="https://cdn.rawgit.com/innovadiv/assets/master/logo-red.png" /></p><h1>Innovadiv - Entertainment Consultation</h1><p>Contact <a href="mailto:lscullark@innovadiv.com">lscullark@innovadiv.com</a> for more details.</p>');
 }).listen(80);
 
 console.log('app running...');
