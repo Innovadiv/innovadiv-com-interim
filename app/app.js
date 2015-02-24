8169058d4ab54e418257faa4d49cc904d6e91266
Matt Lo <mattlo.developer@gmail.com>
2015-02-24 11:38:17 -0600

    added express, scaffolded express app

--- app/app.js
+++ app/app.js
@@ -1,8 +1,60 @@
-var http = require('http');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<p><img style="max-height: 60px;" src="https://cdn.rawgit.com/innovadiv/assets/master/logo-red.png" /></p><h1>Innovadiv - Entertainment Consultation</h1><p>Contact <a href="mailto:lscullark@innovadiv.com">lscullark@innovadiv.com</a> for more details.</p>');
-}).listen(80);
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-console.log('app running...');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
