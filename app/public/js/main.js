9fc748a6ec87714aa69b36f1083db2b2feb93b91
Matt Lo <mattlo.developer@gmail.com>
2015-03-31 21:30:32 -0500

    added featured artists resolved innovadiv/innovadiv-com#18

---
+++ app/public/js/main.js
@@ -0,0 +1,48 @@
+var app = angular.module('innovadiv-marketing', []);
+
+app.controller("featureArtists", function ($scope) {
+    $scope.artists = [
+        {
+            name: "Ron E Polo",
+            image: "ronepolo.jpg",
+            location: "Columbus, OH",
+            genre: "Hip Hop"
+        },
+        {
+            name: "Dr. Auburn Ellis",
+            image: "drauburnellis.jpg",
+            location: "Chicago, IL",
+            genre: "Visual Artist"
+        },
+        {
+            name: "Jake Jude",
+            image: "jakejude.jpg",
+            location: "Chicago, IL",
+            genre: "Electronic, Progressive House"
+        },
+        {
+            name: "Unamused Dave",
+            image: "unamuseddave.jpg",
+            location: "Peoria, IL",
+            genre: "Rock/Punk/Alternative"
+        },
+        {
+            name: "Big Looney",
+            image: "biglooney.jpg",
+            location: "Colorado Springs, CO",
+            genre: "Rap"
+        },
+        {
+            name: "CHISongwriter",
+            image: "chisongwriter.jpg",
+            location: "Peoria, IL",
+            genre: "Hip Hop"
+        },
+        {
+            name: "Marty P",
+            image: "martyp.jpg",
+            location: "Pensacola, FL",
+            genre: "Hip Hop"
+        }
+    ];
+});
